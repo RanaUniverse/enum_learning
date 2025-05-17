2248697668fd69e018cd0c334e12ea24067acb91
RanaUniverse <RanaUniverse321@gmail.com>
2025-05-17 17:36:17 +0530

    make a function reusable for iterable of row

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-text = f"For Now go to my_module.py and run that file."
-print(text)
+"""
+Suppose i want to get 2nd value of the tupe as a list of list
+
+"""
+
+from my_module import MathOperation
+
+
+all_objs = list(MathOperation)
+
+
+if __name__ == "__main__":
+
+    print(all_objs)
--- rana_universe_main.py
+++ rana_universe_main.py
@@ -14,7 +14,22 @@ fruits = [
 items_per_row = 3
 
 fruits_rows = [
-    fruits[i : i + items_per_row] for i in range(0, len(fruits), items_per_row)
+    fruits[i : i + items_per_row]
+    for i in range(
+        0,
+        len(fruits),
+        items_per_row,
+    )
 ]
 
-print(fruits_rows)
+
+def split_list(input_list, items_count):
+
+    return [
+        input_list[i : i + items_count] for i in range(0, len(input_list), items_count)
+    ]
+
+
+if __name__ == "__main__":
+    fruit_rows = split_list(fruits, 3)
+    print(fruit_rows)
